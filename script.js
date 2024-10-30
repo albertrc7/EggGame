b67d55634a7b7f3fa04192b531d62a4ba56419c3
Albert Recuero Campasol <albertrc7573@gmail.com>
2024-10-30 03:20:28 +0100

    First version of the project

---
+++ script.js
@@ -0,0 +1,32 @@
+let imagen1 = document.getElementById('img1');
+let puntu = document.getElementById('puntuacion');
+let imagen2 = 'img/REAL2.png';
+let imagen3 = 'img/REAL3.png'
+let contador = 0;
+
+
+
+imagen1.addEventListener("click", () =>{
+    //Incremento de contador
+    if (contador >= 0 && contador < 20){
+        contador++;
+    }else if(contador >= 20 && contador < 50){
+        contador += 5;
+    }else if(contador >= 50){
+        contador *= 3;
+    }
+
+    //Actualización de la puntuación
+    puntu.innerHTML = contador;
+    
+
+    //Si llega a 20 cambio de imagen
+    if(contador > 19 && contador < 50){
+    imagen1.src = imagen2;
+    }
+
+    //Si llega a 50 cambio de imagen
+    if (contador > 49){
+        imagen1.src = imagen3;
+    }
+})
