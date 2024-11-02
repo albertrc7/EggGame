42f9958e4d14befc6c30028b4dc177f266b98aa3
Albert Recuero Campasol <albertrc7573@gmail.com>
2024-11-02 03:54:20 +0100

    Muchos cambios

--- script.js
+++ script.js
@@ -3,39 +3,67 @@ let puntu = document.getElementById('puntuacion');
 let boton1 = document.getElementById('btn1');
 let imagen2 = 'img/REAL2.png';
 let imagen3 = 'img/REAL3.png';
-let perk = false;
 let contador = 0;
+let intervaloAutomatico;
+let nivelPoder = 0;
+
+const intervalosPorNivel = {
+    1: 1000, 
+    2: 500,  
+    3: 333,  
+    4: 250,  
+    5: 200   
+};
 
 
-
-imagen1.addEventListener("click", () =>{
-    //Incremento de contador
-    if (contador >= 0 && contador < 20){
+imagen1.addEventListener("click", () => {
+ 
+    if (contador >= 0 && contador < 20) {
         contador++;
-    }else if(contador >= 20 && contador < 50){
+    } else if (contador >= 20 && contador < 50) {
         contador += 5;
-    }else if(contador >= 50 && perk){
+    } else if (contador >= 50) {
         contador *= 3;
     }
 
-    //Actualización de la puntuación
-    puntu.innerHTML = contador;
     
+    puntu.innerHTML = contador;
 
-    //Si llega a 20 cambio de imagen
-    if(contador > 19 && contador < 50){
-    imagen1.src = imagen2;
+    actualizarImagen();
+
+    
+});
+
+
+boton1.addEventListener("click", () => {
+    if (nivelPoder < 5) {
+        nivelPoder++;
+        document.getElementById(`nivel${nivelPoder}`).classList.add('active');
+        actualizarIntervalo(); 
     }
+});
 
-    //Si llega a 50 cambio de imagen
-    if (contador > 49){
+
+function incrementarContador() {
+    contador++;
+    puntu.innerHTML = contador;
+    actualizarImagen();
+}
+
+
+function actualizarIntervalo() {
+    if (intervaloAutomatico) clearInterval(intervaloAutomatico);
+
+    intervaloAutomatico = setInterval(incrementarContador, intervalosPorNivel[nivelPoder]);
+
+    
+}
+
+function actualizarImagen(){
+    
+    if (contador >= 20 && contador < 50) {
+        imagen1.src = imagen2;
+    } else if (contador >= 50) {
         imagen1.src = imagen3;
     }
-})
-
-boton1.addEventListener("click", () =>{
-   if(contador >= 50){
-    perk = true;
-   }
-    
-})
+}
